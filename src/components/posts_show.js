f75b3b44623c3196235228772b7a35752afbe0df
AdevaS <adevanir@gmail.com>
2017-01-03 12:48:21 +0000

    Blog exemple finished

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        this.context.router.push('/');
+      });
+  }
 
   render() {
+    const { post } = this.props; // This line is ES6 syntax and it is equivalent to: const post = this.props.post;
+
+    if (!post){
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>Show post {this.props.params.id}</div>
+      <div>
+        <div>
+          <h3>{post.title}</h3>
+          <h6>Category: {post.categories}</h6>
+          <p>{post.content}</p>
+        </div>
+        <div>
+          <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete</button>
+          <Link to="/" className="btn btn-primary">
+            Back
+          </Link>
+        </div>
+      </div>
     );
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
